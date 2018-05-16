8cbbb4b68029ce2fe42188f3c76d2a18521c0a72
Marta Waplak <mwaplak@gmail.com>
2018-05-16 21:36:06 +0200

    Move presentational to src folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router;'
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
+import Navigation from './presentational/navigation.component';
 
 render(
     <Provider store={store}>
         <div>
+            <Router history={hashHistory}>
+                <Route path='/' component={Navigation}>
+                </Route>
+            </Router>
             <DevTools/>
-            <h1>Inicjalizacja projektu</h1>
         </div>
     </Provider>,
     document.getElementById('root')
